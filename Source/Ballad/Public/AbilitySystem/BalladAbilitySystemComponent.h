bc237d95b7f66a865924f36495f43cc556455366
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-07 23:52:29 -0400

    Lecture 19: Ability System Component and Attribute Set

---
+++ Source/Ballad/Public/AbilitySystem/BalladAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "BalladAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLAD_API UBalladAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
