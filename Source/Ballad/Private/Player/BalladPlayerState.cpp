9ff13802ae3004d7657f6b13263df8eb03a3023b
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-07 23:46:49 -0400

    Lecture 18: The Player State

---
+++ Source/Ballad/Private/Player/BalladPlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/BalladPlayerState.h"
+
+ABalladPlayerState::ABalladPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
