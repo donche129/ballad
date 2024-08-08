e33f290650a4b04fc960f3dfbc59164a8bcd3075
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-07 21:08:47 -0400

    Lecture 9: Enhanced Input

--- Source/Ballad/Private/Character/BalladCharacterBase.cpp
+++ Source/Ballad/Private/Character/BalladCharacterBase.cpp
@@ -7,6 +7,9 @@ ABalladCharacterBase::ABalladCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ABalladCharacterBase::BeginPlay()
