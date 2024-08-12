87cae4c3947809c8d33fede0c2029d15d21eb8f1
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-12 01:21:40 -0400

    Lecture 38: Effect Actor Improved

--- Source/Ballad/Private/Actor/BalladEffectActor.cpp
+++ Source/Ballad/Private/Actor/BalladEffectActor.cpp
@@ -3,45 +3,30 @@
 
 #include "Actor/BalladEffectActor.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/BalladAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 ABalladEffectActor::ABalladEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	Sphere->SetupAttachment(GetRootComponent());
-}
-
-void ABalladEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	{
-		const UBalladAttributeSet* BalladAttributeSet = Cast<UBalladAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UBalladAttributeSet::StaticClass()));
-
-		UBalladAttributeSet* MutableBalladAttributeSet = const_cast<UBalladAttributeSet*>(BalladAttributeSet);
-		MutableBalladAttributeSet->SetHealth(BalladAttributeSet->GetHealth() + 25.f);
-		MutableBalladAttributeSet->SetMana(BalladAttributeSet->GetMana() - 25.f);
-		Destroy();
-	}
-}
-
-void ABalladEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 void ABalladEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ABalladEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ABalladEffectActor::EndOverlap);
+}
+
+void ABalladEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
 }
