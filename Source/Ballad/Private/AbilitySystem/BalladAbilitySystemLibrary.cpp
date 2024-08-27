ce36f4edc164a1f1fc2bd90e6ccdcde08a87931f
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-27 00:35:25 -0400

    Lecture 91: Ballad Ability System Blueprint Library

---
+++ Source/Ballad/Private/AbilitySystem/BalladAbilitySystemLibrary.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/BalladAbilitySystemLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/BalladPlayerState.h"
+#include "UI/HUD/BalladHUD.h"
+#include "UI/WidgetController/BalladWidgetController.h"
+
+UOverlayWidgetController* UBalladAbilitySystemLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	if (APlayerController* PC = UGameplayStatics::GetPlayerController(WorldContextObject, 0))
+	{
+		if (ABalladHUD* BalladHUD = Cast<ABalladHUD>(PC->GetHUD()))
+		{
+			ABalladPlayerState* PS = PC->GetPlayerState<ABalladPlayerState>();
+			UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+			UAttributeSet* AS = PS->GetAttributeSet();
+			const FWidgetControllerParams WidgetControllerParams(PC, PS, ASC, AS);
+			return BalladHUD->GetOverlayWidgetController(WidgetControllerParams);
+		}
+	}
+	return nullptr;
+}
