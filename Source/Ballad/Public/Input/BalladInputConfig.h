fb4ba5467ba0da54ecc495301bf1502d140ffa94
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-30 00:52:27 -0400

    Lecture 100: Input Config Data Asset

---
+++ Source/Ballad/Public/Input/BalladInputConfig.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "InputAction.h"
+#include "BalladInputConfig.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBalladInputAction
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	const class UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag = FGameplayTag();
+};
+
+/**
+ * 
+ */
+UCLASS()
+class BALLAD_API UBalladInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+
+	const UInputAction* FindAbilityInputActionForTag(const FGameplayTag& InputTag, bool bLogNotFound = false) const;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FBalladInputAction> AbilityInputActions;
+};
