d214ef037b83549d0459010f06e31c9057e39cdc
Donald Cherestal <donaldcherestal@gmail.com>
2024-08-31 02:00:03 -0400

    Lecture 101: Ballad Input Component

---
+++ Source/Ballad/Public/Input/BalladInputComponent.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "BalladInputConfig.h"
+#include "BalladInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLAD_API UBalladInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+public:
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UBalladInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc);
+};
+
+template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+inline void UBalladInputComponent::BindAbilityActions(const UBalladInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc)
+{
+	check(InputConfig);
+
+	for (const FBalladInputAction& Action : InputConfig->AbilityInputActions)
+	{
+		if (Action.InputAction && Action.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Started, Object, PressedFunc, Action.InputTag);
+			}
+
+			if (ReleasedFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, Action.InputTag);
+			}
+
+			if (HeldFunc)
+			{
+				BindAction(Action.InputAction, ETriggerEvent::Triggered, Object, HeldFunc, Action.InputTag);
+			}
+		}
+	}
+}
