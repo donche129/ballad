e7411fb6e24c965cb4b497954b2ab842f85a4b10
Donald Cherestal <donaldcherestal@gmail.com>
2024-09-04 23:05:43 -0400

    Lecture 110: Ballad Projectile Spell

---
+++ Source/Ballad/Public/AbilitySystem/Abilities/BalladProjectileSpell.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/BalladGameplayAbility.h"
+#include "BalladProjectileSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALLAD_API UBalladProjectileSpell : public UBalladGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
