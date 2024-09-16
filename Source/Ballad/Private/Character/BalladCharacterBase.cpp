847d4f057f841c165df3a8012cb3456dfdea2ed9
Donald Cherestal <donaldcherestal@gmail.com>
2024-09-16 01:16:30 -0400

    Lecture 139: Dissolve Effect

--- Source/Ballad/Private/Character/BalladCharacterBase.cpp
+++ Source/Ballad/Private/Character/BalladCharacterBase.cpp
@@ -50,7 +50,7 @@ void ABalladCharacterBase::MulticastHandleDeath_Implementation()
 	GetMesh()->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
 	
 	GetCapsuleComponent()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	
+	Dissolve();
 }
 
 void ABalladCharacterBase::BeginPlay()
@@ -92,4 +92,20 @@ void ABalladCharacterBase::AddCharacterAbilities()
 	if (!HasAuthority()) return;
 
 	BalladASC->AddCharacterAbilities(StartupAbilities);
+}
+
+void ABalladCharacterBase::Dissolve()
+{
+	if (IsValid(DissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(DissolveMaterialInstance, this);
+		GetMesh()->SetMaterial(0, DynamicMatInst);
+		StartDissolveTimeline(DynamicMatInst);
+	}
+	if (IsValid(WeaponDissolveMaterialInstance))
+	{
+		UMaterialInstanceDynamic* DynamicMatInst = UMaterialInstanceDynamic::Create(WeaponDissolveMaterialInstance, this);
+		Weapon->SetMaterial(0, DynamicMatInst);
+		StartWeaponDissolveTimeline(DynamicMatInst);
+	}
 }
