037ca3caef3eb0fb184fcc6c0ee0d6299b0806d9
Donald Cherestal <donaldcherestal@gmail.com>
2024-10-05 21:08:11 -0400

    Lecture 203: Enemies Multiplayer Testing

--- Source/Ballad/Private/Actor/BalladProjectile.cpp
+++ Source/Ballad/Private/Actor/BalladProjectile.cpp
@@ -56,7 +56,7 @@ void ABalladProjectile::Destroyed()
 void ABalladProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
                                         UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (DamageEffectSpecHandle.Data.IsValid() && DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
+	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor)
 	{
 		return;
 	}
